3dc0b03569872e8768d829d705ae87c6907b8b57
pansf <pansf@xiaopeng.com>
2023-11-20 18:21:18 +0800

    initial commit

---
+++ enanchu_spider/main.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import urllib.request, urllib.error
+import xlwt
+import re
+
+
+class Data_item:
+    pass
+
+
+def main():
+    pass
+
+
+def getData() -> list[Data_item]:
+    pass
+
+
+def getDataItem() -> Data_item:
+    pass
+
+
+def saveData(datalist: list[Data_item], saveDirectory: str):
+    pass
---
+++ env_init.ps1
@@ -0,0 +1,8 @@
+python -m venv ./env
+# update pip
+python -m pip install --upgrade pip
+
+./env/Scripts/activate.ps1
+
+pip install bs4
+pip install xlwt
